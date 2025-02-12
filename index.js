d6e6a0b0b2798f63ae8f40a0661a7028573cf4e7
ivan <ivanromero5520@gmail.com>
2025-02-12 19:57:17 +0100

    Enabling Handlebars as a Template Engine

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const router = require('./routes')
+const { engine } = require('express-handlebars');
+const path = require('path');
+const router = require('./routes');
 
 const app = express();
 
+// Habilitar handlebars como template engine
+app.engine('handlebars', engine({
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'handlebars');
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', router());
 
-app.listen(5000);aaa
+app.listen(5000, () => {
+    console.log('Servidor escuchando en el puerto 5000');
+});
