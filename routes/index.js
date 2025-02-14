74951da5921717c1cf2b2e5ec4decc2bce340d70
ivan <ivanromero5520@gmail.com>
2025-02-14 18:35:59 +0100

    Creating a Master page in handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const homeController = require('../controllers/homeController');
 
 module.exports = () => {
     
-
-    router.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
+    router.get('/', homeController.mostrarTrabajos);
 
     return router;
 }
