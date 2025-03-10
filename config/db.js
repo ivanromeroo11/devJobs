b58a9affa9de94cfc58f53fda8d0f6606acba71b
ivan <ivanromero5520@gmail.com>
2025-03-10 19:57:58 +0100

    Installing Mongoose and Connecting to the Database

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose.connect(process.env.DATABASE)
+    .then(() => console.log('Conexión a MongoDB exitosa'))
+    .catch((error) => console.error('Error de conexión a MongoDB:', error));
+
+mongoose.connection.on('error', (error) => {
+    console.error('Error de conexión a MongoDB:', error);
+});
+
+// Importar los modelos aquí
+// require('../models/YourModel'); // Comentar o eliminar esta línea si no tienes un modelo específico
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
+require('./config/db');
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 
 require('dotenv').config({ path: 'variables.env' });
 
@@ -15,6 +21,16 @@ app.set('view engine', 'handlebars');
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE })
+}));
 
 app.use('/', router());
 
