6dc3d3ba2f93cb44cdf9ada64dc547888ecc96ed
ivan <ivanromero5520@gmail.com>
2025-03-11 02:19:12 +0100

    Creating the Form for new vacancies

---
+++ controllers/vacantesController.js
@@ -0,0 +1,7 @@
+exports.formularioNuevaVacante = (req, res) => {
+    res.render('nueva-vacante', {
+        nombrePagina: 'Nueva Vacante',
+        tagline: 'Llena el formulario y publica tu vacante',
+        
+    })
+};
